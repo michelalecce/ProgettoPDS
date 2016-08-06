c9457c511c32fa334a5fefaac9622ac0eefdaa82
michelalecce <mic.lecce93@gmail.com>
2016-08-06 16:53:17 +0200

    Simple client without sendCommand() to the server

---
+++ ClientProva/ClientProva/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientProva {
+    class Program {
+        static void Main(string[] args) {
+
+            TaskManager tm = new TaskManager();
+            //Connect to localhost and port 1500
+            tm.sockConnect();
+
+            tm.printList();
+        }
+    }
+}
